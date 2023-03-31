70b10cebd6b1c7067fea3c9c886d99d7107d883c
guilherme amorim <cezarguilherme03@gmail.com>
2023-03-31 10:53:28 -0300

    inicialização dos exercicios de fixação

---
+++ Exercicio-01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Escreva um programa que pergunte o dia, mês e ano do aniversário de uma pessoa e diga se a data é válida ou não. Caso não seja, diga o motivo. Suponha que todos os meses tem 31 dias e que estejamos no ano de 2013.
+
+//entrada de dados
+// = perguntar o dia do aniversário
+// = perguntar o mes do aniversário
+// = perguntar o ano do aniversário
+
+//processamento de dados
+// = realizar a verificação dos dias, entre 1 e 31
+// = realizar a verificação dos meses, entre 1 e 12
+// = realizar a verificação do ano, 2013
+
+//saida de dados
+// = mostrar se a data é valida ou não
+
+// int diaAniversário;
+// int mes
+
+Console.WriteLine($"Bem vindo ao meu programa para verificação de data de aniversário!! :)");
+
+Console.WriteLine($"Por faor, digite o dia do seu aniversário: ");
+int diaAniversário = int.Parse(Console.ReadLine());
+
+
+
+
+
+
